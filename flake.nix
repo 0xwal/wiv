5e4231fd180b74a22f71fb5690b9873fed20f402
0xWal <18g3@e.0xw.link>
2026-07-23 13:29:12 +0300

    add flake for nix users ^^

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Displays keypresses on screen on supported Wayland compositors";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+  in {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      wshowkeys = pkgs.stdenv.mkDerivation {
+        pname = "wshowkeys";
+        version = "0.1.0";
+
+        src = self;
+
+        nativeBuildInputs = with pkgs; [ meson ninja pkg-config wayland-scanner ];
+        buildInputs = with pkgs; [
+          cairo
+          libinput
+          libxkbcommon
+          pango
+          udev
+          wayland
+          wayland-protocols
+        ];
+
+        mesonFlags = [ "-Ddevpath=/dev/input/" ];
+
+        meta = with nixpkgs.lib; {
+          description = "Displays keypresses on screen on supported Wayland compositors";
+          homepage = "https://github.com/DreamMaoMao/myshowkey";
+          license = licenses.gpl3Only;
+          platforms = platforms.linux;
+          maintainers = [ ];
+        };
+      };
+    });
+
+    defaultPackage = forAllSystems (system: self.packages.${system}.wshowkeys);
+
+    devShell = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      inputsFrom = [ self.packages.${system}.wshowkeys ];
+      nativeBuildInputs = with pkgs; [ meson ninja pkg-config wayland-scanner ];
+      buildInputs = with pkgs; [
+        cairo
+        libinput
+        libxkbcommon
+        pango
+        udev
+        wayland
+        wayland-protocols
+      ];
+    });
+  };
+}
